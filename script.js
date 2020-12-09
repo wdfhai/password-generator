c2c05dd73faf4e9ac4196da373f21f27a1ff4f28
wdfhai <wd.fhai@outlook.com>
2020-12-08 20:06:39 -0600

    generated password succesfully

--- script.js
+++ script.js
@@ -14,15 +14,10 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 var lC = "abcdefghijklmnopqrstuvwxyz".split("");
-console.log(lC);
 var uC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-console.log(uC);
 var n = "0123456789".split("");
-console.log(n);
 var s = ("!@#$%&").split("");
-console.log(s);
 var lCuC = lC.concat(uC);
-console.log(lCuC);
 var lCn = lC.concat(n);
 var lCs = lC.concat(s);
 var uCn = uC.concat(n);
@@ -34,11 +29,27 @@ var lCns = lCn.concat(s);
 var uCns = uCn.concat(s);
 var lCuCns = lCuCs.concat(n);
 
+var rL = Math.floor(26 * Math.random());
+var rU = Math.floor(26 * Math.random());
+var rN = Math.floor(10 * Math.random());
+var rS = Math.floor(6 * Math.random());
+
+console.log(rU);
+console.log(rL);
+console.log(rN);
+console.log(rS);
+
+console.log(lC[rL]);
+console.log(uC[rU]);
+console.log(n[rN]);
+console.log(s[rS]);
+
 var range = [];
 for (var i = 8; i <= 128; i++) {
     range.push(i);
 }
 console.log(range);
+console.log(range[120]);
 
 alert("Welcome to the Employee Password Generator.");
 alert("This web application will generate a random password between 8 and 128 characters long.")
@@ -51,35 +62,81 @@ var wantUpperCase = confirm("Press OK to include Uppercase letters.");
 var wantNumbers = confirm("Press OK to include numeric characters.");
 var wantSpecial = confirm("Press OK to include the following special characters only. '!@#$%&'");
 
+if ((wantLowerCase) && (wantUpperCase) && (wantNumbers) && (wantSpecial)) {
+  var chosenArray = lCuCns;
+  console.log(chosenArray);
+} else if ((wantLowerCase) && (wantUpperCase) && (wantNumbers)) {
+  var chosenArray = lCuCn;
+  console.log(chosenArray);
+} else if ((wantLowerCase) && (wantUpperCase) && (wantSpecial)) {
+  var chosenArray = lCuCs;
+  console.log(chosenArray);
+} else if ((wantLowerCase) && (wantNumbers) && (wantSpecial)) {
+  var chosenArray = lCns;
+  console.log(chosenArray);
+} else if ((wantUpperCase) && (wantNumbers) && (wantSpecial)) {
+  var chosenArray = uCns;
+  console.log(chosenArray);
+} else if ((wantLowerCase) && (wantUpperCase)) {
+  var chosenArray = lCuC;
+  console.log(chosenArray);
+} else if ((wantLowerCase) && (wantSpecial)) {
+  var chosenArray = lCs;
+  console.log(chosenArray);
+} else if ((wantNumbers) && (wantSpecial)) {
+  var chosenArray = ns;
+  console.log(chosenArray);
+} else if ((wantLowerCase) && (wantNumbers)) {
+  var chosenArray = lCn;
+  console.log(chosenArray);
+} else if ((wantUpperCase) && (wantNumbers)) {
+  var chosenArray = uCn;
+  console.log(chosenArray);
+} else if ((wantUpperCase) && (wantSpecial)) {
+  var chosenArray = uCs;
+  console.log(chosenArray);
+}  else if (wantLowerCase) {
+  var chosenArray = lC;
+  console.log(chosenArray);
+}  else if ((wantUpperCase)) {
+  var chosenArray = uC;
+  console.log(chosenArray);
+}  else if (wantNumbers) {
+  var chosenArray = n;
+  console.log(chosenArray);
+}  else if (wantSpecial) {
+  var chosenArray = s;
+  console.log(chosenArray);
+}  else {
+  alert("A password cannot be generated without at least one set of characters.")
+  console.log("No Array Chosen");
+};
+
 var total = prompt("How many characters do you require? Choose between 8 and 128.");
 var totalInt= parseInt(total);
 
 if (range.includes(totalInt)) {
   alert("Thanks for your input. Press Generate to create your password.");
-  
-  var rL = Math.floor(26 * Math.random());
-  var rU = Math.floor(26 * Math.random());
-  var rN = Math.floor(10 * Math.random());
-  var rS = Math.floor(6 * Math.random());
-  console.log(rU);
-  console.log(rL);
-  console.log(rN);
-  console.log(rS);
 
-  console.log(lC[rL]);
-  console.log(uC[rU]);
-  console.log(n[rN]);
-  console.log(s[rS]);
+  var pw = [];
+  while (pw.length !== totalInt) {
+    // function generate1(){
+    var rn = Math.floor(Math.random() * chosenArray.length);
+    console.log(rn);
+    console.log(chosenArray[rn]);
+    pw.push(chosenArray[rn]);
 
-  var password = [];
-  password.length = totalInt;
-  for (var i=8; i <= totalInt; i++) {
-    password.push(lC[rL]);
-  }
-  console.log(password);
+    }
   
-  
-} 
+} else {
+  alert("That is not a valid choice.")
+};
+console.log(pw);
+var password = pw.join("");
+console.log(password);
+alert("Your password is " + password);
+
+
 
 
 
